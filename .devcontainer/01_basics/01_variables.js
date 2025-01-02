488ea7bbe621c957e3f6a9532847a3ab9ead9610
abhishek <abhishek29112005@gmail.com>
2025-01-02 15:38:00 +0530

    datatypes modified:   .devcontainer/01_basics/01_variables.js         new file:   .devcontainer/01_basics/02_datatypes.js

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 123;
+let accountEmail = "abhishek29112005@gmail.com";
+var accountPassword = "12345";
+accountCity = "noida";
+// accountId = 1213
+// const accountId = 12332 
+accountEmail = "hello";
+console.log(accountEmail);
+// console.log(accountEmail,accountPassword,accountCity,accountId)
+// console.table([accountEmail,accountId,accountPassword,accountCity])
+// alert(3+3);
