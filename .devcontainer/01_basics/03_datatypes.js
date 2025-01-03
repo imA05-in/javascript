249afc3e56480ad199620f35e2a0d8f951132b3b
abhishek <abhishek29112005@gmail.com>
2025-01-03 20:44:54 +0530

    datatypes summary

---
+++ .devcontainer/01_basics/03_datatypes.js
@@ -0,0 +1,16 @@
+let a = "hello";
+let b = "hi";
+// console.log(a+b);
+// console.log(a%b);
+
+let c = true;
+let d = false;
+// console.log(c%d)
+
+let e = null;
+let f = null;
+// console.log(e%f)
+
+let g = undefined;
+let h = undefined;
+console.log(g*a)
---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,13 @@
+let value = Symbol(123);
+let value2 = Symbol(123);
+console.log(value === value2);
+
+// {
+//     name :"abhishek",
+//     age: 19
+// }
+// console.log(name)
+
+naem = 12;
+naem = 13
+console.log(naem)
