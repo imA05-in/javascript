c99e133d1852ca1009083628f54c99cda7c336ef
abhishek <abhishek29112005@gmail.com>
2025-01-03 10:03:44 +0530

    datatypes

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -1,3 +1,19 @@
-let a = 2;
-let b = typeof a;
-console.log(b);
+// let a = 2;
+// let b = typeof a;
+
+// let c = "hello";
+
+// console.log({ b, a, c });
+
+// let abc = Number(c)
+// c = Number(c)
+// console.log(c, typeof c)
+// console.log(abc, typeof abc)
+let t = 'true';
+let q = Number(t)
+let w = true;
+let w1 = Number(w)
+let num = -1;
+let num1 = Boolean(num)
+console.log(num1, typeof(num1))
+// console.log(w1, typeof w1, typeof w)
