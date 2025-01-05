0ee0ccbc80b3e0d0601ff2a288fce3d0bc0d569f
abhishek <abhishek29112005@gmail.com>
2025-01-05 18:18:36 +0530

    nums and maths

---
+++ .devcontainer/01_basics/06_nums_maths.js
@@ -0,0 +1,11 @@
+let score = new Number(100);
+// console.log(typeof(score));
+// score = score.toString();
+// console.log(typeof(score));
+
+// console.log(Number.MIN_VALUE)
+
+const min = 10;
+const max = 20;
+// console.log(Math.random.maxValue())
+console.log(Math.random(min,max))
