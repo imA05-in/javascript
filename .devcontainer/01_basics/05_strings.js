724b2923dbcdb54bc566f4c5c8db04f67bbdbf09
abhishek <abhishek29112005@gmail.com>
2025-01-05 15:14:25 +0530

    new file:   .devcontainer/01_basics/05_strings.js

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,56 @@
+const name0 = "Abhishek0";
+let age = 19;
+const name = new String('abhishek')
+// console.log(`Hello my name is ${name} and age is ${age}`)
+// console.log(name0.length)
+// console.log(name.length)
+
+
+// concat
+const str1 = "hello";
+const str2 = "world";
+// console.log(str1.concat(' ', str2));
+
+//endswith
+const question01 = "is this hulalala ";
+// console.log(question01.endsWith(""));
+
+//includes
+
+const word = "fox fox fox ";
+// console.log(word.includes("x"));
+
+//lastIndexOf();
+// console.log(word.lastIndexOf("fox"));
+
+//regex;
+const sentence02 = "the fox Is Nice. Hello WORLD"
+// console.log(sentence02.match(/[A-Z]/g))
+
+ const name1 = "abhishek";
+//  console.log(name1.codePointAt(name1[1]))
+
+ //replace
+ const paragrapph = "I think Ruth's dog is cuter than you r dog";
+//  console.log(`${paragrapph.replace("dog", "cake")} (;`);
+
+//  console.log(paragrapph.search("dog"));
+
+
+//split
+const str = 'The quick brown fox jumps over the lazy dog.';
+// console.log(str.split('b'))
+
+//toString
+const str3 = 3
+// console.log(str3.toString())
+
+//trim
+const str4 = "   Hello world    ";
+// console.log(str4.trimStart());
+console.log(str4.valueOf());
+
+
+//valueOf
+const str5 = new String("Hello duniya. ");
+console.log(str5.valueOf());
