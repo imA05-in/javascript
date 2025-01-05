deadaf253e36ed57a602bb66f9c7b6125753d391
abhishek <abhishek29112005@gmail.com>
2025-01-05 19:08:57 +0530

    dates

---
+++ .devcontainer/01_basics/dates.js
@@ -0,0 +1,28 @@
+let date = new Date();
+// console.log(date.toString());  
+// console.log(date.toJSON());
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toUTCString());
+// console.log(date.getTimezoneOffset());
+let myDate = new Date(2025 ,10,29);
+// console.log(myDate.toDateString());
+let myDate01 = new Date("11-29-2005");
+// console.log(myDate01.toString())
+
+let time = Date.now();
+// console.log(time)
+// console.log(myDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+
+
+
+
+let dateMy = new Date();
+console.log(dateMy.getDay())
+console.log(dateMy.getMonth())
+console.log(dateMy.getFullYear())
+console.log(dateMy.getDate())
