77794b2a344a8e49d4d1d500d526bd65cfecf607
abhishek <abhishek29112005@gmail.com>
2025-01-07 17:23:48 +0530

    02 basics

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+const arr = [1, 2, 34, 5];
+// console.log(arr[3]);
+
+const arry2 = new Array(1, 2, 3, 4, 5,);
+// console.log(arry2);
+for (let i = 0; i < arry2.length; i++) {
+    // console.log(`${arry2[i]}   `);
+}
+
+//array methods
+const arr3 = [1,2,3,4,5,6,7];
+// arr3.push(4);
+// arr3.pop();
+arr3.unshift(9);
+arr3.shift();
+console.log(arr3);
