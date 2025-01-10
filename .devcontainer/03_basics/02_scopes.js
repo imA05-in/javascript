60522c7c1030789dfbe66923506440e4f388011a
abhishek <abhishek29112005@gmail.com>
2025-01-10 17:58:48 +0530

    scopes.js

---
+++ .devcontainer/03_basics/02_scopes.js
@@ -0,0 +1,28 @@
+function one (){
+    const name = "Abhishek";
+    function two (){
+    const site = "portfolio"
+    // console.log(name);
+    }
+    // console.log(site)
+    two();
+}
+one();
+
+
+
+// const r = add(4)
+// console.log(r)
+
+function add(num){
+    return (num +1)
+}
+
+
+const result = function addTwo(num){
+    return num + 2;
+}
+// addTwo(5)
+console.log(result(5));
+
+
