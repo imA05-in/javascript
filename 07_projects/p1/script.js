977b5958c9049c0ab6aa062d4de27dcbfc59a9b2
abhishek <abhishek29112005@gmail.com>
2025-01-18 11:37:12 +0530

    color changing project

--- 07_projects/p1/script.js
+++ 07_projects/p1/script.js
@@ -1,2 +1,37 @@
-const buttons = document.querySelectorAll(".button")
-console.log(buttons)
+const buttons = document.querySelectorAll(".button");
+console.log(buttons);
+const body = document.querySelector("body");
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+    switch (e.target.id) {
+      case "grey":
+        body.style.backgroundColor = e.target.id;
+        body.style.color = "black";
+        document.querySelector(".button").style.border = "2px solid black";
+        break;
+      case "white":
+        body.style.backgroundColor = e.target.id;
+        body.style.color = "black";
+        document.querySelector(".button").style.border = "2px solid black";
+        break;
+      case "blue":
+        body.style.backgroundColor = e.target.id;
+        body.style.color = "white";
+        document.querySelector(".button").style.border = "2px solid black";
+        break;
+      case "yellow":
+        body.style.backgroundColor = e.target.id;
+        body.style.color = "black";
+        document.querySelector(".button").style.border = "2px solid black";
+        break;
+      case "black":
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+        document.querySelector("#black").style.border = "2px solid white";
+        break;
+    }
+  });
+});
