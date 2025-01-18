abf6105f46c08f29671a8d3384be73190ce3d61b
abhishek <abhishek29112005@gmail.com>
2025-01-18 14:13:44 +0530

    bmi calculator

---
+++ 07_projects/p2 Bmi cal/script.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("form")
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const height = parseFloat(document.querySelector('#height input').value)
+    const weight = parseFloat(document.querySelector("#weight input").value)
+    const result = document.querySelector(".result")
+    if(height === ''||height == 0 ||isNaN(height)){
+        result.innerHTML = "not a valid height value!"
+        if(weight === ''||weight == 0 ||isNaN(weight)){
+            result.innerHTML = "not a valid height and weight value!"
+        }
+    }
+    else if(weight === ''||weight == 0 ||isNaN(weight)){
+        result.innerHTML = "not a valid weight value!"
+    }
+    else{
+        const bmi = weight/((height*height)/10000)
+        result.innerHTML = bmi
+    }
+})
